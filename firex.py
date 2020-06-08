cc13ff81164bab9a9aaa3bc5fb1232fbd7b6e1d1
unknown <andypythonappdeveloper@gmail.com>
2020-06-08 14:18:11 +1000

    Fixed errors

--- firex.py
+++ firex.py
@@ -2,7 +2,7 @@ import fire
 
 while True:
     text = input(">>> ")
-    result, error = fire.run(text)
+    result, error = fire.run("<stdin>", text)
 
     if error: print(error.as_string())
     else: print(result)
