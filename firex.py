aa29c17c23efa6f7148caa92da855ef939642fc2
unknown <andypythonappdeveloper@gmail.com>
2020-06-14 17:35:56 +1000

    Added Function Colors

--- firex.py
+++ firex.py
@@ -1,7 +1,24 @@
 import fire
 
+from pygments.lexers.html import HtmlLexer
+from prompt_toolkit.shortcuts import prompt
+from prompt_toolkit.lexers import PygmentsLexer
+
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+class DiffLexer(RegexLexer):
+    name = 'Diff'
+    aliases = ['diff']
+    filenames = ['*.diff']
+
+    tokens = {
+        'root': [
+            (r'function', Generic.Heading), # Blue
+        ]
+    }
 while True:
-    text = input(">>> ")
+    text = prompt(">>> ", lexer=PygmentsLexer(DiffLexer))
     result, error = fire.run("<stdin>", text)
 
     if error: print(error.as_string())
